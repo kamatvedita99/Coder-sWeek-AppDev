6f9000f965d6eb927347534a2950576e62d34717
sid <15kamat1999@gmail.com>
2020-06-04 14:09:13 +0530

    Added output screen snaps from my phone

--- Day 3/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Day 3/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
-var id=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,4 +12,4 @@ var id=0
         }
 
     }
-}
+
